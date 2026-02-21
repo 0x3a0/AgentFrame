04bc3c124bd28fdecf9e0910de4cae9a8d67d2d1
Ice1and <1185330343@qq.com>
2026-02-21 23:00:45 +0800

    baseagent

--- src/agent/base_agent.py
+++ src/agent/base_agent.py
@@ -1,6 +1,41 @@
-from src.models import BaseModel
+from json import loads
+from typing import Optional, Callable
+
+from src.model import BaseModel
 
 
-class BaseAgent(BaseModel):
+class BaseAgent:
     """ BaseAgent """
-    pass
+    def __init__(
+        self,
+        *,
+        model: BaseModel,
+        tools: Optional[list[Callable]] = None
+    ):
+        # 模型实例
+        self.model = model
+
+        # 工具
+        if tools:
+            self.tools = self._registry_tool(tools)
+
+
+    def _registry_tool(self, tools: list[Callable]):
+        """ 注册工具实例 """
+        tool_descriptions = []
+        for tool in tools:
+            tool_descriptions.append(loads({
+                "type": "function",
+                "name": tool.__name__,
+                "description": tool.__doc__.strip(),
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        
+                    }
+                }
+            }))
+        print(tool_descriptions)
+
+    def run(self):
+        pass
