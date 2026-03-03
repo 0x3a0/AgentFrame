ae10cdfbb6b5a0da22753f2d040fa701805e42e1
Ice1and <1185330343@qq.com>
2026-03-03 22:14:14 +0800

    添加pydantic支持

--- src/tool/tool_registry.py
+++ src/tool/tool_registry.py
@@ -1,22 +1,39 @@
-class Tool:
+from typing import Type
+import pprint
+
+from pydantic import BaseModel
+
+
+class ToolRegistry:
     """ 
-    Tool 类
+    ToolRegistry 类
     用于注册和管理工具实例
     该类提供了一个 register 装饰器方法，用于注册工具实例
     """
     def __init__(self):
-        self.tools = []
+        self.tools: list[dict[str, str]] = []
 
-    def register(self, func):
-        """ 提供注册工具的装饰器方法 """
-        self.tools.append(
-            {
-                "name": func.__name__,
-                "instruction": func.__doc__.strip(),
-                "arguments": func.__annotations__
-            }
-        )
-        return func
+    def register(self, *, description: str, parameterModel: Type[BaseModel]):
+        """ 
+        注册 tool 的装饰器方法
+        根据传入的参数模型生成符合 OpenAI 函数调用格式的 tool model
+        :param description: 工具函数的描述
+        :param parameterModel: 工具函数的参数模型
+        """
+        def wrapper(func):
+            self.tools.append(
+                {
+                    "name": func.__name__,
+                    "description": description,
+                    "parameters": parameterModel.model_json_schema()
+                }
+            )
+
+            return func
+
+        return wrapper
 
     def print_tools(self):
-        print(self.tools)
+        """ 列出当前已注册的工具 """
+        for tool in self.tools:
+            pprint.pprint(tool)
