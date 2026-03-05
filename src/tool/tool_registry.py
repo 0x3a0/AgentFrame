b67810f68cc4aa118ab3dc0dd507641700a2b1a9
Ice1and <1185330343@qq.com>
2026-03-05 21:37:53 +0800

    完善生成模型可调用的工具列表

--- src/tool/tool_registry.py
+++ src/tool/tool_registry.py
@@ -1,5 +1,5 @@
-from typing import Type
-import pprint
+from copy import deepcopy
+from typing import Type, Callable
 
 from pydantic import BaseModel
 
@@ -11,29 +11,52 @@ class ToolRegistry:
     该类提供了一个 register 装饰器方法，用于注册工具实例
     """
     def __init__(self):
-        self.tools: list[dict[str, str]] = []
+        self.tools: dict[str, dict] = {}
 
-    def register(self, *, description: str, parameterModel: Type[BaseModel]):
+    def register(self, description: str, *, Model: Type[BaseModel]):
         """ 
         注册 tool 的装饰器方法
         根据传入的参数模型生成符合 OpenAI 函数调用格式的 tool model
         :param description: 工具函数的描述
-        :param parameterModel: 工具函数的参数模型
+        :param Model: 工具函数的参数模型
         """
         def wrapper(func):
-            self.tools.append(
-                {
-                    "name": func.__name__,
-                    "description": description,
-                    "parameters": parameterModel.model_json_schema()
+            model_json_schema = Model.model_json_schema()
+            # print(model_json_schema)
+            args = model_json_schema["properties"].keys()
+            for arg in args:
+                self.tools[func.__name__] = {
+                    "func": func,
+                    "type": "function",
+                    "function": {
+                        "name": func.__name__,
+                        "description": description,
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                str(arg): {
+                                    "type": model_json_schema["properties"][arg]["type"],
+                                    "description": model_json_schema["properties"][arg]["description"]
+                                }
+                            },
+                            "required": model_json_schema["required"]
+                        }
+                    }
                 }
-            )
-
+                
             return func
 
         return wrapper
 
-    def print_tools(self):
-        """ 列出当前已注册的工具 """
-        for tool in self.tools:
-            pprint.pprint(tool)
+    def get_registered_tools(self):
+        """ 获取已注册的工具列表 """
+        return self.tools
+    
+    def get_model_callable_tools(self):
+        """ 获取模型可调用的工具列表 """
+        tools = []
+        for tool in deepcopy(self.tools).values():
+            del tool['func']
+            tools.append(tool)
+        
+        return tools
