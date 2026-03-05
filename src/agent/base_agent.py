565769b82a1393d25bf5e25b9cd6d16100d5a10d
Ice1and <1185330343@qq.com>
2026-03-05 21:39:17 +0800

    添加agent启动入口

--- src/agent/base_agent.py
+++ src/agent/base_agent.py
@@ -1,4 +1,3 @@
-from json import loads
 from typing import Optional, Callable
 
 from src.model import BaseModel
@@ -11,7 +10,7 @@ class BaseAgent:
         *,
         model: BaseModel,
         system_prompt: Optional[str] = None,
-        tool: Optional[list[Callable]] = None
+        tools: Optional[list[Callable]] = None
     ):
         # 模型实例
         self.model = model
@@ -19,12 +18,12 @@ class BaseAgent:
         # 系统提示
         if system_prompt:
             model.system_prompt = system_prompt
-            self.system_prompt = system_prompt
-        else:
-            self.system_prompt = model.system_prompt
 
         # 工具
         self.tools = tools
 
-    def run(self):
-        pass
+    def run(self, messages: list[dict[str, str]]):
+        """ 运行智能体 """
+        model_tools = self.tools.get_model_callable_tools()
+        resp = self.model.invoke(messages, tools=model_tools)
+        return resp
